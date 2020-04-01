1cc924bfb538d5b1cb608ca14d6d338a2e543781
kalyankumarp <kalyanpaladugula@gmail.com>
2020-04-01 00:04:15 -0500

    IML_01_April_up1

--- Homeworks/HW3/.ipynb_checkpoints/neuralnetwork-checkpoint.py
+++ Homeworks/HW3/.ipynb_checkpoints/neuralnetwork-checkpoint.py
@@ -14,176 +14,35 @@ def sigmoid_derivative(p):
 class NeuralNetwork:
 
     #Do not change this function header
-    def __init__(self,x=[],y=[],numLayers=2,numNodes=2, numOutputs = 1, eta=0.001,maxIter=10000):
+    def __init__(self,x=[[]],y=[],numLayers=2,numNodes=2,eta=0.001,maxIter=10000):
         self.data = x
         self.labels = y
         self.nLayers = numLayers
         self.nNodes = numNodes
-        self.numOutputs = numOutputs
         self.eta = eta
         self.maxIt = maxIter
-        self.weights = [{"weights":np.random.rand(len(x[0]),numNodes)}] #create the weights from the inputs to the first layer
-        for i in range(self.nLayers-1):
-            self.weights.append({"weights":np.random.rand(numNodes,numNodes)}) #create the random weights between internal layers
-            
-        self.weights.append({"weights":np.random.rand(numOutputs,numNodes)}) #create weights from final layer to output node
-        self.outputs = np.zeros(y.shape)
+        #self.weights = [np.random.rand(len(x[0]),numNodes)] #create the weights from the inputs to the first layer
+        #for each of the layers
+            #self.weights.append(np.random.rand(numNodes,numNodes) #create the random weights between internal layers
+            #self.weights.append(np.random.rand(numNodes,1)) #create weights from final layer to output node
+            #self.outputs = np.zeros(y.shape)
+            #self.train()
         self.a = 1 #this is how you define a non-static variable
 
-    def update(self, g =[]):
-        for i in range(len(self.weights)):
-            for j in range(len(self.weights[i]['weights'])):
-                for k in range(len(self.weights[i]['weights'][j])):
-                    self.weights[i]['weights'][j][k] += self.eta *g[k]
-                
     def train(self):
         #Do not change this function header
-        
-        for e in  range(self.maxIt):
-            #print("-----------------------------------------------------------------------------------------------------------------------")
-            #print("Epoch " + str(e))
-            #print("The weights are " + str(self.weights))
-            for i in range(len(self.data)):
-                g =[]
-                #print(self.data[i])
-                #print(self.labels[i])
-                g = self.backprop(self.labels[i], self.data[i], self.weights, self.nLayers, self.nNodes) 
-                #print(g)
-
-                
-                self.update(g)
-        #print(self.weights)
-        return self.weights
-
+        return 0.0
 
     def predict(self,x=[]):
         #Do not change this function header
-        #print('\n')
-        #print("Predict Function")
-        prev = x
-        for j in range(self.nLayers):
-            l = []
-            s = []
-            for m in range(self.nNodes):
-                s.append(np.matmul(self.weights[j]["weights"][m], prev))
-            for k in s:
-                l.append(sigmoid(k))
-            prev = np.asarray(l)
-            s =[]
-        for k in range(self.numOutputs):
+        return 0.0
 
-            p = np.matmul(prev,np.transpose(self.weights[j+1]["weights"][k]))
-            s.append(p)
-        q = []
-        for i in range(len(s)):
-            q.append(sigmoid(s[i]))
-        return q
-
-    def feedforward(self, data, weights, nLayers, nNodes):
+    def feedforward(self):
         #This function is likely to be very helpful, but is not necessary
-        #print('\n')
-        #print("Feedforward Function")
-        r =[]
-        prev = data
-        r.append(prev)
+        return 0.0
 
-        t =[]
-
-        for j in range(nLayers):
-            l = []
-            s = []
-            for m in range(nNodes):
-                #print(weights[j]["weights"][m])
-                s.append(np.matmul(weights[j]["weights"][m], prev))
-            
-            for k in s:
-                l.append(sigmoid(k))
-            prev = np.asarray(l)
-            #print("Prev = " + str(prev))
-            t.append(s)
-            r.append(l)
-        
-        s =[]
-        for k in range(self.numOutputs):
-            
-            #print(weights[j+1]["weights"][k])
-            p = np.matmul(prev,np.transpose(weights[j+1]["weights"][k]))
-            s.append(p)
-        #p = sigmoid(p)
-        t.append(s)
-        #print("P = "  + str(p))
-        #print("t = " + str(t))
-        
-        
-        return t,r
-
-    def backprop(self, d, data, weights, nLayers, nNodes):
+    def backprop(self):
         #This function is likely to be very helpful, but is not necessary
-        #print('\n')
-        #print("BackProp")
-        g =[]
-        der = []
-        t,r = self.feedforward(data, weights, nLayers, nNodes)
-        
-        q = self.predict(data)
-        #r.append(q)
-        #print("r = " + str(r))
-        #print("Q " + str(q))
-        for i in range(nLayers):
-            a =[]
-            for m in range(nNodes):
-                a.append(sigmoid_derivative(t[i][m]))
-                #print(a)
-            der.append(a)
-        diff = []
-        s =[]
-        for i in range(self.numOutputs):
-            s.append(sigmoid_derivative(q[i]))
-            diff.append(d - q[i])
-        der.append(s)
-        #print("der " + str(der))
-        #print("diff = " + str(diff))
-        for i in reversed(range(len(weights))):
-            #print(i)
-            layer = weights[i]
-            #print("Layer " + str(layer))
-            errors = []
-            if i != len(weights)-1:
-                for j in range(len(layer['weights'])):
-                    error = 0
-                    #print('\n')
-                    #print('\n')
-                    #print(j)
-                    #print(weights[i + 1]['weights'])
-                    for k in range(len(weights[i + 1]['weights'])):
-                        #print("k " + str(k))
-                        #print(weights[i + 1]['weights'][k][j])
-                        #print(weights[i + 1]['s'][k])
-                        error = (weights[i + 1]['weights'][k][j] * weights[i + 1]['s'][k])
-                    errors.append(error)
-                    #print(errors)
-            else:
-                errors.append(diff[0])
-                #print(errors)
-            layer['s'] = []
-            for j in range(len(layer['weights'])):                
-                #print("der[i] " + str(der[i]))
-                #print("errors " + str(errors))
-                layer['s'].append(errors[j]*der[i][j])
-            #print("For layer " + str(i) + " the weights are " + str(self.weights))
-            
-        #print(self.weights)       
-        for j in range(len(self.weights)):
-            for k in range(len(self.weights[j]['weights'])):
-                for m in range(len(self.weights[j]['weights'][k])):
-                    g.append(-(r[j][m])*self.weights[j]['s'][k])
-                    
-                
-            
-        
-        g = np.asarray(g)   
-        #print('\n')
-        return g
-
+        return 0.0
 
 
--- Homeworks/HW3/HW3_P1.py
+++ Homeworks/HW3/HW3_P1.py
@@ -5,134 +5,213 @@
 import numpy as np
 np.random.seed(100)
 
-def sigmoid(t):
+
+# Sigmoid is the activation Function
+def af(t):
     return 1 / (1 + np.exp(-t))
 
-def sigmoid_derivative(p):
-    return p * (1 -p)
+def af_derivative(p):
+    return af(p) * (1 -af(p))
 
+def mse(x, d, w, nLayers, nNodes):
+    c = 0
+    n = len(x)
+    for i in range(0,n):
+        prev = x[i] 
+            
+        for j in range(nLayers):
+            u =[]
+            p =[]
+            for k in range(nNodes):
+                t = np.dot(w[j]['weights'][k], prev)
+                u.append(t)
+            for l in u:
+                p.append(af(l))
+            p.append(1)
+            prev = np.asarray(p)
+        y = np.dot(w[nLayers]["weights"][0], prev)
+        c += (d[i] - y)**2
+            
+    return c/n
+
+#np.random.seed(100)
 class NeuralNetwork:
-
-    #Do not change this function header
-    def __init__(self,x=[],y=[],numLayers=2,numNodes=2, numOutputs = 1, eta=0.001,maxIter=10000):
-        self.data = x
+    
+    def __init__(self,x=[],y=[],numLayers=2,numNodes=2, numOutputs = 1, eta=0.001,maxIter=10000, ep = 0):
+        #self.data = x
         self.labels = y
         self.nLayers = numLayers
         self.nNodes = numNodes
         self.numOutputs = numOutputs
         self.eta = eta
         self.maxIt = maxIter
+        self.ep = ep
+        
         #self.g = len(self.data[0])*numLayers + numLayers*numNodes + numNodes*numOutputs
-        self.weights = [{"weights":np.random.rand(numNodes, len(x[0]))}] #create the weights from the inputs to the first layer
-        for i in range(self.nLayers-1):
-            self.weights.append({"weights":np.random.rand(numNodes,numNodes)}) #create the random weights between internal layers
-            
-        if self.nLayers > 0:
-            
-            self.weights.append({"weights":np.random.rand(numOutputs,numNodes)}) #create weights from final layer to output node
-        self.outputs = np.zeros(y.shape)
+        newdata = []
+        for i in range(len(x)):
+            newdata.append(np.append(x[i],1))     
+        self.data = newdata
+        self.weights = [{"weights":np.random.uniform(low =-2, high = 2, size = (self.nNodes, len(self.data[0])))}] 
+        for i in range(self.nLayers):
+            self.weights.append({"weights":np.random.uniform(low =-2, high = 2, size = (self.nNodes,self.nNodes+1))})             
+        if self.nLayers >= 0:            
+            self.weights.append({"weights":np.random.uniform(low =-2, high = 2, size = (self.numOutputs,self.nNodes+1))})
 
-                
+        #return self.weights
+        #self.train()
+        
+        
     def train(self):
-        #np.random.shuffle(self.data)
-        print(self.weights)
-        for e in  range(2 *self.maxIt):
-            
+        
+        np.random.shuffle(self.data)
+        print(self.data)
+        temp_eta = self.eta
+#         temp2 = mse(self.data, self.labels, self.weights, self.nLayers+1, self.nNodes)
+#         print(temp2)
+#         print(f) 
+        e = 0
+        obj =[]
+        epoch = []
+        cos = 100000000
+
+        while e < self.maxIt:
+            prev = cos
             for i in range(len(self.data)):
-                self.backprop(self.labels[i], self.data[i], self.weights, self.nLayers, self.nNodes) 
+                self.backprop(self.labels[i], self.data[i]) 
                 for m in range(len(self.weights)):
                     for j in range(len(self.weights[m]['weights'])):
                         for k in range(len(self.weights[m]['weights'][j])):
                             self.weights[m]['weights'][j][k] -= self.eta *self.weights[m]['g'][j][k]
-        return self.weights
 
+
+            cos = mse(self.data, self.labels, self.weights, self.nLayers+1,self.nNodes)
+            epoch.append(e)
+            obj.append(cos)
+            e += 1
+
+#             if cos >= prev:
+#                 self.eta = 0.9*self.eta
+#                 obj =[]
+#                 epoch = []
+#                 e = 0
+#                 obj.append(cos)
+#                 epoch.append(e)
+#                 cos = 100000000
+# #                 self.weights = temp_weights
+# #                 print(temp_weights)
+                
+#                 if self.eta <= 0.00001:                    
+#                     self.eta = temp_eta  
+# #                     obj =[]
+# #                     epoch = []
+# #                     e = 0
+# #                     cos = 100000000
+#                     self.weights = [{"weights":np.random.uniform(low =-1, high = 1, size = (self.nNodes, len(self.data[0])))}] 
+#                     for i in range(self.nLayers):
+#                         self.weights.append({"weights":np.random.uniform(low =-1, high = 1, size = (self.nNodes,self.nNodes+1))})             
+#                     if self.nLayers >= 0:            
+#                         self.weights.append({"weights":np.random.uniform(low =-1, high = 1, size = (self.numOutputs,self.nNodes+1))})
+                          
+#             elif cos < prev:
+#                 epoch.append(e)
+#                 obj.append(cos)
+#                 e += 1
+        #print(self.weights)
+        return 0.0   
+       
 
     def predict(self,x=[]):
-        prev = x
-        for j in range(self.nLayers):
+        prev = np.append(x,1)
+        for j in range(self.nLayers+1):
             l = []
             s = []
             for m in range(self.nNodes):
                 s.append(np.matmul(self.weights[j]["weights"][m], prev))
             for k in s:
-                l.append(sigmoid(k))
+                l.append(af(k))
+            l.append(1)
             prev = np.asarray(l)
+        
         s =[]
-
-        p = np.matmul(self.weights[self.nLayers]["weights"][0], prev)
+        p = np.matmul(self.weights[self.nLayers+1]["weights"][0], prev)
         s.append(p)
-        q = sigmoid(p)
+        q = af(p)
         return q
-
-    def feedforward(self, data, weights, nLayers, nNodes):
+        
+    def feedforward(self, data):
         r =[]
         prev = data
         r.append(prev)
-
         t =[]
 
-        for j in range(nLayers):
+        for j in range(self.nLayers+1):
             l = []
             s = []
-            for m in range(nNodes):
-                s.append(np.dot(weights[j]["weights"][m], prev))
+            for m in range(self.nNodes):
+                #print(self.weights[j]["weights"][m])
+                s.append(np.dot(self.weights[j]["weights"][m], prev))
             
             for k in s:
-                l.append(sigmoid(k))
+                l.append(af(k))
+            l.append(1)
             prev = np.asarray(l)
             t.append(s)
             r.append(l)
-        
+            
         s =[]
-        p = np.dot(weights[nLayers]["weights"][0], prev)
+        p = np.dot(self.weights[self.nLayers+1]["weights"][0], l)
         s.append(p)
-        #p = sigmoid(p)
-        t.append(s)       
+        q = af(p)
+        t.append(s)        
         
-        return t,r
+        return t,r, q
 
-    def backprop(self, d, data, weights, nLayers, nNodes):
+    def backprop(self, d, data):
         der = []
-        t,r = self.feedforward(data, weights, nLayers, nNodes)
+        t,r, q= self.feedforward(data)
+#         print('\n')
+#         print("t = " + str(t))
+#         print('\n')
+#         print('r = ' + str(r))  
+        #q = self.predict(data)
         
-        q = self.predict(data)
-        for i in range(nLayers):
+        for i in range(self.nLayers+1):
             a =[]
-            for m in range(nNodes):
-                a.append(sigmoid_derivative(t[i][m]))
+            for m in range(self.nNodes):
+                a.append(af_derivative(t[i][m]))
             der.append(a)
         
         diff = []
         s =[]
-        s.append(sigmoid_derivative(t[nLayers][0]))
+        s.append(af_derivative(t[self.nLayers+1][0]))
         diff.append(d - q)
         der.append(s)
-        for i in reversed(range(len(weights))):
-            layer = weights[i]
+        for i in reversed(range(len(self.weights))):
+            layer = self.weights[i]
             errors = []
-            if i != len(weights)-1:
+            if i != len(self.weights)-1:
                 for j in range(len(layer['weights'])):
-                    error = 0                    
-                    for k in range(len(weights[i + 1]['weights'])):
-                        error = (weights[i + 1]['weights'][k][j] * weights[i + 1]['s'][k])
+                    error = 0
+                    
+                    for k in range(len(self.weights[i + 1]['weights'])):
+                        error += (self.weights[i + 1]['weights'][k][j] * self.weights[i + 1]['s'][k])
                     errors.append(error)
             else:
-                errors.append(diff[0])             
+                errors.append(diff[0])
             layer['s'] = []
             for j in range(len(layer['weights'])):
                 layer['s'].append(errors[j]*der[i][j])
-       
+#         print('\n')
+        #print(weights)
+        #print(f)
         for j in range(len(self.weights)):
             layer = self.weights[j]
             layer['g'] = []
             for k in range(len(self.weights[j]['weights'])):
                 s =[]
                 for m in range(len(self.weights[j]['weights'][k])):
-                    s.append((-(r[j][m])*self.weights[j]['s'][k])*2)
+                    s.append(((-(r[j][m])*self.weights[j]['s'][k])*2)/len(self.data))
                 layer['g'].append(s)
-        #print(self.weights)
 
         return 0.0
-
-
-
